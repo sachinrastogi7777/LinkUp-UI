0b8aed62770827d84e00b246eafacfe788e4947a
sachinrastogi7777 <sachinrastogi23101997@gmail.com>
2025-10-22 20:09:04 +0530

    made BASE_URL dynamic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -44,33 +44,35 @@ const NavBar = ({ totalRequests }) => {
                 </Link>
             </div>
             {userData &&
-                <div className="flex gap-2 items-center ml-auto">
+                <div className="flex gap-5 items-center ml-auto">
                     {currentPath !== '/requests' && (
                         <button onClick={() => navigate('/requests')} className="btn btn-primary">{`Requests (${totalRequests})`}</button>
                     )}
-                    <p className='flex items-center px-2.5'>{`Welcome, ${userData.firstName}`}</p>
-                    <div className="dropdown dropdown-end mx-5">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="user-profile-pic"
-                                    src={userData.profileImage}
-                                />
+                    <div className='flex items-center'>
+                        <p className='flex items-center px-2'>{`Welcome, ${userData.firstName}`}</p>
+                        <div className="dropdown dropdown-end mx-5">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="user-profile-pic"
+                                        src={userData.profileImage}
+                                    />
+                                </div>
                             </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                            >
+                                <li>
+                                    <Link to='/profile' className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </Link>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <li onClick={handleLogOut}><Link>Logout</Link></li>
+                            </ul>
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-                        >
-                            <li>
-                                <Link to='/profile' className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </Link>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li onClick={handleLogOut}><Link>Logout</Link></li>
-                        </ul>
                     </div>
                 </div>
             }
